7e87c5aff235bfb5b55f5fe1a863faf1c34e3900
derocious <150651852+Derocious@users.noreply.github.com>
2024-03-15 04:13:42 -0700

    Update dero_miner.sh
    Edited to use configuration file

--- dero_miner.sh
+++ dero_miner.sh
@@ -1,8 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+
+WALLET=""
+POOL_ADDRESS=""
+source config.conf
 CPU_THREADS=$(nproc)
-POOL_ADDRESS="community-pools.mysrv.cloud:10300"
-WALLET="dero1qyfgvm4y5wgsng2hhfg8hggqere4qu7rswwxaglj0ftcl9zegnhykqq6d4rcf"
 HANSEN_DOWNLOAD_LINK="https://github.com/Hansen333/Hansen33-s-DERO-Miner/releases/download/Version-0.6/hansen33s-dero-miner-android-arm64.tar.gz"
 DERO_CLI_MINER_LINK="https://github.com/deroproject/derohe/releases/download/Release140/dero_linux_arm7.tar.gz"
 
@@ -37,7 +39,7 @@ else
     chmod +x dero-miner*
     echo "#!/data/data/com.termux/files/usr/bin/bash" >> start_miner.sh
     echo " " >> start_miner.sh
-    echo "$(pwd)/dero-miner* --wallet-address $WALLET --daemon-rpc-address 213.171.208.37:10300 --mining-threads $CPU_THREADS" >> start_miner.sh
+    echo "$(pwd)/dero-miner* --wallet-address $WALLET --daemon-rpc-address $POOL_ADDRESS --mining-threads $CPU_THREADS" >> start_miner.sh
     chmod +x start_miner.sh
     MINING_SCRIPT="$(pwd)/start_miner.sh"
     echo "tmux new-session \"bash $MINING_SCRIPT\"" >> $HOME/.bashrc
